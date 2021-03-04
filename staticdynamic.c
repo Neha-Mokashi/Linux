6844fab9ac8b3e404d377a157345f03f32fba67d
Neha-Mokashi <79127646+Neha-Mokashi@users.noreply.github.com>
2021-03-04 10:19:11 +0530

    Create staticdynamic.c

---
+++ staticdynamic.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+   int ar[2][3];
+   int *arc[2];
+   
+   int ar1[3];
+   int ar2[3];
+   
+   arc[0]=ar1;
+   arc[1]=ar2;
+   
+   ar1[0]=12;
+   ar1[1]=13;
+   ar1[2]=14;
+   
+   ar2[0]=15;
+   ar2[1]=16;
+   ar2[2]=17;
+   printf("helo world\n");
+   
+   for(int i=0;i<2;i++)
+   {
+       for(int j=0;j<3;j++)
+       {
+           printf("%d\t",arc[i][j]);
+       }
+       printf("\n");
+   }
+    return 0;
+    
+}
